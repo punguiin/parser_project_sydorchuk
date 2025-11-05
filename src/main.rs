b027e56bfb40ba3bf771d9de739d1b31965bfe96
Nazar Sydorchuk <n.sydorchuk@newtonideas.com>
2025-11-05 22:59:52 +0200

    added unit test

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,13 @@ fn main() -> anyhow::Result<()> {
     dbg!(pair);
     Ok(())
 }
+
+#[test]
+fn is_sum() -> anyhow::Result<()> {
+    let pair = Grammar::parse(Rule::input, "(12+34)")?
+        .next()
+        .ok_or_else(|| anyhow!("No pair"))?;
+    assert_eq!(pair.as_rule(), Rule::input);
+    assert_eq!(pair.as_span().start(), 0);
+    Ok(())
+}
